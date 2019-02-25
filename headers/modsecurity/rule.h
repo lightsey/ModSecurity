b58cf259b231817d0076d53155c18dc5d569d5aa
Felipe Zimmerle <felipe@zimmerle.org>
2019-02-25 11:32:43 -0300

    Rule: isMarker is no longer necessary

--- headers/modsecurity/rule.h
+++ headers/modsecurity/rule.h
@@ -93,9 +93,6 @@ class Rule {
         return *m_fileName + ":" + std::to_string(m_lineNumber);
     }
 
-
-    virtual bool isMarker() { return false; }
-
  private:
     std::shared_ptr<std::string> m_fileName;
     int m_lineNumber;
